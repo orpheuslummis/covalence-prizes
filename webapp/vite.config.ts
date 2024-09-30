4864092c8622823298b910c5902cc25ea4c269e4
Orpheus Lummis <o@horizonomega.org>
2024-09-30 15:40:41 -0500

    fix: fleek compatibility

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
