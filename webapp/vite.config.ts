556cc47fde63399fb48bfde26c87dc7431f8564c
Orpheus Lummis <o@horizonomega.org>
2024-09-30 15:26:36 -0500

    fix: fleek compatibility

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -7,6 +7,7 @@ import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfil
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
